bbff490ee4902cc9ef0e38e8a4c462ff4ac8bb24
Wilken003 <wilkenbogonko@gmail.com>
2025-11-26 16:25:21 +0300

    Add files via upload

---
+++ Invoice.java
@@ -0,0 +1,23 @@
+public class Invoice {
+    public static void main(String[] args) {
+        System.out.println("\n\tBill To:\t\t\t\t\t\t\t\t\t\t\t\t\tShip To:\n");
+        System.out.println("\tC026-01-0775/2024\t\t\t\t\t\t\t\t\t\t\tMasaku John Wambua");
+        System.out.println("\tNo 23/2, SBI Colony.\t\t\t\t\t\t\t\t\t\tNo 23/2, SBI Colony.");
+        System.out.println("\tRagavendra Nagar,Chennai - 600124\t\t\t\t\t\t\tRagavendra Nagar, Chennai - 600124");
+        System.out.println("\tGSTIN:33APFSDF1ZV\t\t\t\t\t\t\t\t\t\t\tGSTIN:33APFSDF1ZV");
+        System.out.println("\tContact: +91-7869825463\t\t\t\t\t\t\t\t\t\tContact: +91-7869825463");
+        System.out.println("\tEmail: purchase-team@thendral.com\t\t\t\t\t\t\tEmail: purchase-team@thendral.com\n");
+        System.out.println("\t\t\t\tPayment Date: 7 days from date of delivery\t\tPayment Terms: 100% against invoice\n");
+        System.out.println("S.No\tProduct Code\tProduct Name\t\t\tHSN code\t Quantity\tUnits\t  Rate\t   Tax\t       Amount");
+        System.out.println(" 1\t\t\t105\t\t\tSurf Excel 5kg \t\t\t34019011\t    20\t\t nos     600.00\t    5%\t\t 12600.00");
+        System.out.println(" 2\t\t\t109\t\t\tRin 1kg \t\t\t\t34019011\t    25\t\t nos\t  85.00\t    5%\t\t  2231.00");
+        System.out.println(" 3\t\t\t115\t\t\tHamam soap 150g \t\t34011100\t    25\t\t nos\t  60.00\t    5%\t\t  1575.00");
+        System.out.println(" 4\t\t\t120\t\t\tLux Soap 150g \t\t\t34011100\t    30\t\t nos\t  53.00\t    5%\t\t   443.00");
+        System.out.println(" 5\t\t\t125\t\t\tDove soap 125g \t\t\t34011100\t    25\t\t nos\t  75.00\t    5%\t\t  1968.00");
+        System.out.println(" 6\t\t\t129\t\t\tVim bar 200g \t\t\t08109010\t    20\t\t nos\t  15.00\t    5%\t\t   315.00");
+        System.out.println(" 7\t\t\t131\t\t\tPepsodent 150g \t\t\t33061020\t    30\t\t nos\t  85.00\t   18%\t      3009.00\n");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t  Total\t\t     22141.00");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t  Discounts\t\t   141.00");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t  Grand total\t 22000.00");
+    }
+}
